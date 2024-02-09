c6ef1012f49483eeab7596e8759be6f0629e3b44
Naka Masato <masatonaka1989@gmail.com>
2024-02-09 16:05:00 +0800

    update

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,6 +7,7 @@ import pytest
 from slack_sdk.signature import SignatureVerifier
 
 VERIFIER = SignatureVerifier(os.environ["SIGNING_SECRET"])
+EVENT_PATH = "/slack/events"
 
 
 @pytest.fixture()
@@ -37,65 +38,67 @@ def timestamp():
 
 
 @pytest.fixture()
-def generate_signature(timestamp):
-    def generate_signature_from_data(data):
-        return VERIFIER.generate_signature(timestamp=timestamp, body=json.dumps(data))
+def headers(timestamp):
+    def generate_headers(data: str | bytes):
+        return {
+            "X-Slack-Request-Timestamp": timestamp,
+            "X-Slack-Signature": VERIFIER.generate_signature(
+                timestamp=timestamp, body=data
+            ),
+            "Content-Type": "application/json",
+        }
 
-    return generate_signature_from_data
+    return generate_headers
 
 
 # @patch("langchain_openai.ChatOpenAI")
 def test_request_example_invalid(flask_cli):
-    response = flask_cli.post("/slack/events")
+    response = flask_cli.post(EVENT_PATH)
     assert response.status_code == 403
     assert response.get_data(as_text=True) == "Invalid request"
 
 
-def test_url_verification(flask_cli, timestamp, generate_signature):
+def test_url_verification(flask_cli, headers):
     """
     url_verification
     https://github.com/nakamasato/slack-gpt/blob/695d6ca73ae65629be29f9d8159c26fdb2c7a815/tests/test_main.py#L44-L74
     """
-    data = {
-        "type": "url_verification",
-        "challenge": "challenge",
-    }
+    data_str: str = json.dumps(
+        {
+            "type": "url_verification",
+            "challenge": "challenge",
+        }
+    )
 
     response = flask_cli.post(
-        "/slack/events",
-        data=json.dumps(data),  # json=data or json=json.dumps(data) doesn't work
-        headers={
-            "X-Slack-Request-Timestamp": timestamp,
-            "X-Slack-Signature": generate_signature(data=data),
-            "Content-Type": "application/json",
-        },
+        EVENT_PATH,
+        data=data_str,  # json=data or json=json.dumps(data) doesn't work
+        headers=headers(data=data_str),
     )
     assert response.status_code == 200
     assert response.get_json()["challenge"] == "challenge"
 
 
 @patch("slack_gpt.main.slack")
-def test_message(slack_mock, flask_cli, timestamp, generate_signature):
+def test_message(slack_mock, flask_cli, headers):
     """
     when receiving message event
     add reaction
     """
-    data = {
-        "type": "message",
-        "event": {
-            "channel": "C000000",  # set in DEDICATED_CHANNELS
-            "ts": "1707454484.055569",
-        },
-    }
+    data_str: str = json.dumps(
+        {
+            "type": "message",
+            "event": {
+                "channel": "C000000",  # set in DEDICATED_CHANNELS
+                "ts": "1707454484.055569",
+            },
+        }
+    )
 
     response = flask_cli.post(
-        "/slack/events",
-        data=json.dumps(data),  # json=data or json=json.dumps(data) doesn't work
-        headers={
-            "X-Slack-Request-Timestamp": timestamp,
-            "X-Slack-Signature": generate_signature(data=data),
-            "Content-Type": "application/json",
-        },
+        EVENT_PATH,
+        data=data_str,  # json=data or json=json.dumps(data) doesn't work
+        headers=headers(data=data_str),
     )
     assert response.status_code == 200
     slack_mock.reactions_add.assert_called_with(
